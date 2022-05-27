466f72306d0f8e8b0c98973c7d2a70335086e87a
CATboardBETA <catboardbeta@gmail.com>
2022-05-27 11:52:06 -0400

    hotfix in main

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ mod qiish;
 
 fn main() {
 
-    println!("{}", qiish::rewrite_relative_dir(Path::new("~/qiishhome").to_owned(), Path::new("/Users/will").to_owned(), Path::new("/Users/will/qiishhome").to_owned()).unwrap().to_str().unwrap());
+    println!("{}", qiish::rewrite_relative_dir(Path::new("~/qiishhome").to_owned(), Path::new("/Users/will"), Path::new("/Users/will/qiishhome")).unwrap().to_str().unwrap());
 
     if let Ok(qiish) = qiish::Qiish::new(Path::new("/Users/will/qiishhome")) {
         qiish
